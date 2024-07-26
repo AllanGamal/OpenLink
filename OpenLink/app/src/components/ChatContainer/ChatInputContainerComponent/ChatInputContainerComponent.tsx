d28aad10f11d71b9c8047640f191f1d613b4dce1
Allan Gamal <63316423+AllanGamal@users.noreply.github.com>
2024-07-26 11:22:11 +0200

    basic ui set up.  The history/answer functionality needs to be connected to the UI

--- OpenLink/app/src/components/ChatContainer/ChatInputContainerComponent/ChatInputContainerComponent.tsx
+++ OpenLink/app/src/components/ChatContainer/ChatInputContainerComponent/ChatInputContainerComponent.tsx
@@ -25,54 +25,14 @@ function ChatInputContainerComponent({ onSendMessage }: Props) {
   }
 
 
-
-
   const handleSendClick = () => {
     const apiUrl = 'http://localhost:8001/message';
 
 
     if (message !== '') {
-      setIsLoading(true);
       onSendMessage({ text: message, type: 'user'});
+      onSendMessage({ text: "message", type: 'bot'});
       setMessage('');
-      axios.post(apiUrl, { message, is_english: isEnglish }, { timeout: 1000*60*60 }) // 1 hour timeout, default seems to be 1min
-        .then(response => {
-          setIsLoading(false);
-          if (response.status === 200) {
-            console.log('Message sent');
-            // go ghrough the source list and print out the source
-            console.log("test")
-            console.log(response.data.answer);
-            console.log(response.data.pageContents);
-            let newMetadata = response.data.metadata;
-            let newPageContents = response.data.pageContents;
-            
-            // go through the pageContents list and check if there is less that 100 characters
-            for (let i = 0; i < newPageContents.length; i++) {
-              if (response.data.pageContents[i].length < 100) {
-                newPageContents.splice(i, 1);
-                newMetadata.splice(i, 1);
-                i--;
-              }
-            }
-
-            onSendMessage({ text: response.data.answer, type: 'bot', metadata: newMetadata, pageContents: newPageContents});
-            console.log(response.data);
-            
-          } else {
-            onSendMessage({ text: "Failed to send message: " + response.statusText, type: 'bot'});
-            setIsLoading(false);
-            setMessage('');
-          }
-        })
-
-        .catch(error => {
-          onSendMessage({ text: "Failed to send message, could not connect to server.", type: 'bot'});
-          setIsLoading(false);
-          setMessage('');
-          console.error(error);
-        });
-
     }
   };
 
